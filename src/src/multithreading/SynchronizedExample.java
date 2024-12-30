dbd905cbb34a8ecc7617556913dca9eb9b16a761
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2024-12-30 22:09:19 +0530

    SynchronizedExample

---
+++ src/src/multithreading/SynchronizedExample.java
@@ -0,0 +1,42 @@
+package multithreading;
+
+class Counter {
+    private int count = 0;
+
+    public void increment() {
+        synchronized (this) {  // Synchronize on the current object
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class SynchronizedExample {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Counter Value: " + counter.getCount());
+    }
+}
+
